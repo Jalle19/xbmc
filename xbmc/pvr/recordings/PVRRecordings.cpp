754782c0fbf5d7e47c87274b2c2b37c2585cf51e
Sam Stenvall <neggelandia@gmail.com>
2014-11-14 11:07:34 +0200

    [pvr] fix renaming recordings
    The recording tag in the file item is a copy, we can't operate on that.
    The update will be sent successfully to the backend in either case but
    the GUI won't be updated accordingly unless we change the correct item.

--- xbmc/pvr/recordings/PVRRecordings.cpp
+++ xbmc/pvr/recordings/PVRRecordings.cpp
@@ -234,15 +234,13 @@ bool CPVRRecordings::DeleteRecording(const CFileItem &item)
 
 bool CPVRRecordings::RenameRecording(CFileItem &item, std::string &strNewName)
 {
-  bool bReturn = false;
-
-  if (!item.IsPVRRecording())
+  CPVRRecordingPtr tag = GetByFileItem(item);
+  if (!tag)
   {
     CLog::Log(LOGERROR, "CPVRRecordings - %s - cannot rename file: no valid recording tag", __FUNCTION__);
-    return bReturn;
+    return false;
   }
-
-  CPVRRecording* tag = item.GetPVRRecordingInfoTag();
+  
   return tag->Rename(strNewName);
 }
 
